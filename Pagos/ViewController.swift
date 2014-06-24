1c8c4ef8450260fc435d2f8c2cbf446491086076
DFectuoso <santiago1717@gmail.com>
2014-06-24 16:56:38 -0500

    Initial Commit

---
+++ Pagos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pagos
+//
+//  Created by Santiago Zavala de la Vega on 6/24/14.
+//  Copyright (c) 2014 Dfectuoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
